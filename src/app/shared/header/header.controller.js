a71a9d616cd341163b3eee277655525f9e6d99a6
Joel Hernandez <involvmnt@gmail.com>
2015-12-10 19:01:18 +0100

    Adds the header base files

---
+++ src/app/shared/header/header.controller.js
@@ -0,0 +1,20 @@
+/**
+ * @author joel
+ * 10-12-15
+ */
+
+(function () {
+    'use strict';
+
+    /**
+     * The Header Controller
+     *
+     * @ngInject
+     */
+    var HeaderController = function () {
+
+    };
+
+    angular.module('kpnAssignment').controller('kpnAssignment.headerController', HeaderController);
+
+}());
