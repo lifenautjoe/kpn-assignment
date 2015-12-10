d4b5fb5885726b92aecee11f330a9afc16fdf516
Joel Hernandez <involvmnt@gmail.com>
2015-12-10 20:04:54 +0100

    Adds bootstrap nav bar as header template and populates its items from the available states

--- src/app/shared/header/header.controller.js
+++ src/app/shared/header/header.controller.js
@@ -11,8 +11,24 @@
      *
      * @ngInject
      */
-    var HeaderController = function () {
+    var HeaderController = function ($state) {
+        // Store injected services
+        this.$state = $state;
 
+        // Bootstrap
+        this.refreshStates();
+    };
+
+    /**
+     * Refreshes the header states
+     */
+    HeaderController.prototype.refreshStates = function () {
+        // Retrieve states
+        var states = this.$state.get();
+        // First item is the abstract state
+        states.shift();
+        // Store
+        this.states = states;
     };
 
     angular.module('kpnAssignment').controller('kpnAssignment.headerController', HeaderController);
