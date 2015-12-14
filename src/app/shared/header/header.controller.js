ebf395aa85bc48f20bd0d4ea9eb034fb80dcc52d
Joel Hernandez <involvmnt@gmail.com>
2015-12-14 02:19:37 +0100

    Creates a way to hide the displayed state items in the header through a data attribute and tests it

--- src/app/shared/header/header.controller.js
+++ src/app/shared/header/header.controller.js
@@ -11,12 +11,25 @@
      *
      * @ngInject
      */
-    var HeaderController = function ($state) {
+    var HeaderController = function ($state,lodash) {
         // Store injected services
         this.$state = $state;
+        this.lodash = lodash;
 
         // Bootstrap
         this.refreshStates();
+    };
+
+    /**
+     * Gets the application states
+     * @returns {*}
+     */
+    HeaderController.prototype.getStates = function () {
+        var states = this.$state.get();
+        // First item is the abstract state
+        states.shift();
+
+        return states;
     };
 
     /**
@@ -24,11 +37,12 @@
      */
     HeaderController.prototype.refreshStates = function () {
         // Retrieve states
-        var states = this.$state.get();
-        // First item is the abstract state
-        states.shift();
-        // Store
-        this.states = states;
+        var states = this.getStates();
+
+        // Store the non hidden states
+        this.states = this.lodash.filter(states, function (state) {
+            return !state.data || !state.data.hidden;
+        });
     };
 
     angular.module('kpnAssignment').controller('kpnAssignment.headerController', HeaderController);
--- src/app/shared/header/header.spec.js
+++ src/app/shared/header/header.spec.js
@@ -4,32 +4,167 @@
  */
 
 
-(function() {
+(function () {
     'use strict';
-    describe('Header', function() {
-        var el;
-        var vm;
 
+    describe('Header', function () {
         beforeEach(module('kpnAssignment'));
-        beforeEach(inject(function($compile, $rootScope) {
-            el = angular.element('<ka-header></ka-header>');
-            $compile(el)($rootScope.$new());
-            $rootScope.$digest();
-            vm = el.controller('kaHeader');
-        }));
 
-        it('should be compiled', function() {
-            expect(el.html()).not.toEqual(null);
-        });
 
-        describe('states attribute', function () {
-            it('should be defined', function () {
-                expect(vm.states).toBeDefined();
+        /*        describe('Directive', function () {
+         var el;
+
+         beforeEach(inject(function($compile, $rootScope) {
+         el = angular.element('<ka-header></ka-header>');
+         $compile(el)($rootScope.$new());
+         $rootScope.$digest();
+         }));
+
+         it('should be compiled', function() {
+         expect(el.html()).not.toEqual(null);
+         });
+
+         });*/
+
+        describe('Controller', function () {
+            var vm;
+            var states = [
+                {name: 'One'}, {name: 'Two'}, {name: 'Three'}
+            ];
+
+            beforeEach(inject(function ($controller, $state, lodash) {
+
+                // Stub the $state service
+                spyOn($state, 'get').and.callFake(function () {
+                    return lodash.clone(states);
+                });
+
+                // Initiliaze the controller with the spied services
+                vm = $controller('kpnAssignment.headerController', {
+                    $state: $state,
+                    lodash: lodash
+                });
+            }));
+
+            describe('()', function () {
+                it('should refresh states', function () {
+                    expect(vm.states).toBeDefined();
+                    expect(vm.states.constructor === Array).toBe(true);
+                });
             });
 
-            it('should be an array', function () {
-                expect(vm.states.constructor === Array).toBe(true);
+            describe('getStates()', function () {
+
+                it('should get the $state states without the first result', function () {
+                    var retrievedStates = vm.getStates();
+                    // Only one item
+                    expect(retrievedStates.length == states.length - 1).toBe(true);
+                    expect(states).toEqual(jasmine.arrayContaining(retrievedStates));
+                });
+
             });
+
+            describe('refreshStates()', function () {
+
+                describe('when no state is hidden', function () {
+
+                    var retrievedStates = [
+                        {
+                            name : 'Some'
+                        },
+                        {
+                            name : 'State'
+                        }
+                    ];
+
+                    beforeEach(function () {
+                        spyOn(vm, 'getStates').and.returnValue(retrievedStates);
+                    });
+
+                    it('should set all the states', function () {
+                        vm.refreshStates();
+                        expect(vm.states.length == retrievedStates.length);
+                        expect(vm.states).toEqual(jasmine.arrayContaining(retrievedStates));
+                    });
+                });
+
+                describe('when states are hidden', function () {
+
+                    var shownStates = [
+                        {
+                            name : 'Something'
+                        },
+                        {
+                            name : 'Test'
+                        }
+                    ];
+
+                    var hiddenStates = [
+                        {
+                            name : 'Else',
+                            data : {
+                                hidden : true
+                            }
+                        }
+                    ];
+
+                    var retrievedStates = shownStates.concat(hiddenStates);
+
+                    beforeEach(function () {
+                        spyOn(vm, 'getStates').and.returnValue(retrievedStates);
+                    });
+
+                    it('should set only the non hidden states', function () {
+                        vm.refreshStates();
+                        expect(vm.states).not.toEqual(jasmine.arrayContaining(hiddenStates));
+                        expect(vm.states).toEqual(jasmine.arrayContaining(shownStates));
+                    });
+                });
+
+            });
+
         });
     });
+    /*
+     describe('Header', function() {
+     var el;
+     var vm;
+
+
+     beforeEach(module('kpnAssignment'));
+     beforeEach(inject(function($compile, $rootScope,$state) {
+     el = angular.element('<ka-header></ka-header>');
+     $compile(el)($rootScope.$new());
+     $rootScope.$digest();
+     vm = el.controller('kaHeader');
+     }));
+
+     it('should be compiled', function() {
+     expect(el.html()).not.toEqual(null);
+     });
+
+     describe('()', function () {
+     it('should refresh states', function () {
+     expect(vm.states).toEqual(products);
+     });
+     });
+
+     describe('getStates()', function () {
+
+     });
+
+     describe('refreshStates()', function () {
+
+     });
+
+     describe('states attribute', function () {
+     it('should be defined', function () {
+     expect(vm.states).toBeDefined();
+     });
+
+     it('should be an array', function () {
+     expect(vm.states.constructor === Array).toBe(true);
+     });
+     });
+     });*/
 }());
